68d0f9db3c02f6fab429514d4508c4a3dedd0d72
Amr Yasser <amr.yassr.2@gmail.com>
2024-10-10 11:34:45 +0300

    added student sign up

--- students/urls.py
+++ students/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from students.views import liststudents
+from students.views import liststudents, signup_view
+
 urlpatterns = [
 
-path('',liststudents.as_view(),name='students.index')
-
+    path('', liststudents.as_view(), name='students.index'),
+    path('signup/', signup_view, name='signup'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from students.models import students
@@ -7,3 +7,21 @@ class liststudents(ListView):
     model=students
     template_name='students_index.html'
     context_object_name='students'
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST.get('phone')
+        track = request.POST.get('track')
+        address = request.POST.get('address')
+
+        # Create a new student record
+        student = students(name=name, email=email, phone=phone, track=track, address=address)
+        student.save()
+
+        # Redirect to a success page or display a success message
+        return redirect('students.index')
+
+    return render(request, 'students_signup.html')
